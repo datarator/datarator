2d7d97d2dde0101883ece00086656524d0888196
Peter Butkovic <butkovic@gmail.com>
2016-08-03 23:37:54 +0200

    test: introduced for column: `row_index`

---
+++ columnrowindex_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func TestColumnRowIndexValue(t *testing.T) {
+	var tests = []struct {
+		inColumn  TypedColumn
+		inContext Context
+		outValue  string
+	}{
+		{
+			inColumn: ColumnRowIndex{
+				Options: ColumnOptionsRowIndex{},
+				Column:  Column{},
+			},
+			inContext: Context{
+				CurrentRowIndex: 0,
+			},
+			outValue: "0",
+		},
+		{
+			inColumn: ColumnRowIndex{
+				Options: ColumnOptionsRowIndex{},
+				Column:  Column{},
+			},
+			inContext: Context{
+				CurrentRowIndex: 100,
+			},
+			outValue: "100",
+		},
+	}
+
+	for _, test := range tests {
+		actual, _ := test.inColumn.Value(test.inContext)
+		if actual != test.outValue {
+			t.Fatalf("Expected: %v\nActual: %v", test.outValue, actual)
+		}
+	}
+}
