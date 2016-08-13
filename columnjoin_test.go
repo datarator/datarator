7aea8fb0120553dc84ffa9dab119f79c479e63ab
Peter Butkovic <butkovic@gmail.com>
2016-08-13 07:35:31 +0200

    test: introduced for column: `join`

---
+++ columnjoin_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestColumnJoinValue(t *testing.T) {
+	var tests = []struct {
+		inColumn  TypedColumn
+		inContext Context
+		outValue  string
+	}{
+		{
+			inColumn: ColumnJoin{
+				Column: Column{
+					TypedColumns: []TypedColumn{
+						ColumnConst{
+							Payload: ColumnConstPayload{
+								Value: "Hello",
+							},
+						},
+						ColumnConst{
+							Payload: ColumnConstPayload{
+								Value: "datarator",
+							},
+						},
+					},
+				},
+				Payload: ColumnJoinPayload{
+					Separator: " ",
+				},
+			},
+			inContext: Context{},
+			outValue:  "Hello datarator",
+		},
+		{
+			inColumn: ColumnJoin{
+				Column: Column{
+					TypedColumns: []TypedColumn{
+						ColumnConst{
+							Payload: ColumnConstPayload{
+								Value: "Hello",
+							},
+						},
+						ColumnConst{
+							Payload: ColumnConstPayload{
+								Value: "datarator",
+							},
+						},
+					},
+				},
+			},
+			inContext: Context{},
+			outValue:  "Hellodatarator",
+		},
+	}
+
+	for _, test := range tests {
+		actual, _ := test.inColumn.Value(&test.inContext)
+		matched, _ := regexp.MatchString(test.outValue, actual)
+		if !matched {
+			t.Fatalf("Expected: %v\nActual: %v", test.outValue, actual)
+		}
+	}
+}
